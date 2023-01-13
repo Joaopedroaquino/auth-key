b5ea4db2f25c5505332dab12199f5c5eb3e55dd9
joaopedroaquino <aqerhack@gmail.com>
2023-01-13 08:31:32 -0300

    config express server

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res, next) {
+  res.json('Hello World');
+});
+
+
+app.listen(3000, '0.0.0.0', ()=> {
+  console.log('Server is running')
+})
