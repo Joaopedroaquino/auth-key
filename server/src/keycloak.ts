b80947292d2473e7180130bb775a34eeca9cf4c1
joaopedroaquino <aqerhack@gmail.com>
2023-01-13 09:40:19 -0300

    ref: keycloak-config

--- server/src/keycloak.ts
+++ server/src/keycloak.ts
@@ -5,7 +5,7 @@ export const memoryStore = new session.MemoryStore()
 
 const config: KeycloakConnect.KeycloakConfig = {
     realm: 'my-realm',
-    "auth-server-url": 'http://localhost:8080/auth',
+    "auth-server-url": 'http://localhost:8080/auth/',
     resource: 'express-web',
     "confidential-port": 0,
     "ssl-required": "external"
